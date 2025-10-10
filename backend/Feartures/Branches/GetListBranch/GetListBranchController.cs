fdaac958f6e82ceeb5ff2a8c3404bdce838c08fa
thuanvct1.longan@gmail.com <thuanvct1.longan@gmail.com>
2025-10-10 21:38:26 +0700

    BE: Get List branch

---
+++ backend/Feartures/Branches/GetListBranch/GetListBranchController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Feartures.Branches.GetListBranch
+{
+    [ApiController]
+    [Route("api/branches")]
+    public class GetListBranchController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public GetListBranchController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _mediator.Send(new GetListBranchQuery());
+            if (result.IsSuccess)
+                return Ok(result.Value);
+            else
+                return BadRequest(result.Errors);
+        }
+    }
+}
