36261357b3c192c2ec5a4358ce588927bf9cb3a6
thuanvct1.longan@gmail.com <thuanvct1.longan@gmail.com>
2025-10-10 11:29:14 +0700

    be create user api

---
+++ backend/Feartures/Users/CreateUserController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Feartures.Users
+{
+    [ApiController]
+    [Route("api/users")]
+    public class CreateUserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CreateUserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateUserRequest request)
+        {
+            var result = await _mediator.Send(new CreateUserCommand(request));
+            if (result.IsSuccess)
+                return Ok(new { user_id = result.Value });
+            else
+                return BadRequest(result.Errors);
+        }
+    }
+}
