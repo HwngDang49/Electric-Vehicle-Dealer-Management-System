292968901fc10a6740082d08a3c4a61027057df2
Alex Dang <Hungthanh4904@gmail.com>
2025-10-13 01:27:19 +0700

    BE: create new feature: GetQuoteDetails

---
+++ backend/Feartures/SalesDocuments/Quotes/GetQuoteDetails/GetQuoteDetailsValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace backend.Feartures.SalesDocuments.Quotes.GetQuoteDetails
+{
+    public class GetQuoteDetailsValidator : AbstractValidator<GetQuoteByIdQuery>
+    {
+        public GetQuoteDetailsValidator()
+        {
+            RuleFor(x => x.SalesDocId)
+            .GreaterThan(0).WithMessage("Id must be greater than 0.");
+            // DealerId lấy từ JWT, nhưng vẫn đảm bảo > 0
+            RuleFor(x => x.DealerId)
+                .GreaterThan(0).WithMessage("Dealer context is required.");
+        }
+    }
+}
