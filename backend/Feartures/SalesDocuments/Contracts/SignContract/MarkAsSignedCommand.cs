49c4dd2ddf1f329ac19ecdee67d58e6532389bd2
Alex Dang <Hungthanh4904@gmail.com>
2025-10-13 22:30:16 +0700

    BE: Create new feature - Sign Contract

---
+++ backend/Feartures/SalesDocuments/Contracts/SignContract/MarkAsSignedCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+using Ardalis.Result;
+using MediatR;
+
+namespace backend.Feartures.SalesDocuments.Contracts.SignContract
+{
+    public sealed class MarkAsSignedCommand : IRequest<Result<DateTime>>
+    {
+        [JsonIgnore]
+        public long SalesDocId { get; set; }
+
+        [JsonIgnore]
+        public long DealerId { get; set; }
+    }
+}
