b78fa281bbe81e73c68db8e871daf13bebf9d48d
Võ Đức Huy <huyvdse180565@fpt.edu.vn>
2025-10-22 14:31:20 +0700

    FE: fix dealerManager

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -40,7 +40,16 @@ import testDriveApiService from "./testDriveApi";
 import roleBasedApiService from "./roleBasedApi";
 import pricebookItemsApiService from "./pricebookItemsApi";
 import productsWithPricingApiService from "./productsWithPricingApi";
-import * as poDataMapper from "./poDataMapper";
+import {
+  mapBackendPoToFrontend,
+  mapBackendPoDetailToFrontend,
+  mapBackendPoItemToFrontend,
+  mapFrontendToBackendPo,
+  formatPrice,
+  formatDate,
+  getStatusDisplayText,
+  getStatusColorClass,
+} from "./poDataMapper";
 
 // Export all services
 export {
@@ -84,9 +93,16 @@ export {
   roleBasedApiService,
   pricebookItemsApiService,
   productsWithPricingApiService,
-  
+
   // Data Mappers
-  ...poDataMapper,
+  mapBackendPoToFrontend,
+  mapBackendPoDetailToFrontend,
+  mapBackendPoItemToFrontend,
+  mapFrontendToBackendPo,
+  formatPrice,
+  formatDate,
+  getStatusDisplayText,
+  getStatusColorClass,
 };
 
 // Default export for convenience
